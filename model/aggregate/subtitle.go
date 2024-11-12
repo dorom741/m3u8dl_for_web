5275f6302023a0e86db19243142b8d59a1409302
your name <example@example.com>
2024-11-12 18:51:01 +0800

    修复配置文件读取问题

--- model/aggregate/subtitle.go
+++ model/aggregate/subtitle.go
@@ -5,15 +5,15 @@ import (
 )
 
 type SubtitleInput struct {
-	Provider       string    `json:"provider"`
-	InputPath      string    `json:"inputPath"`
-	SavePath       string    `json:"savePath"`
-	Reader         io.Reader `json:"-"`
-	Prompt         string    `json:"prompt"`
-	Temperature    float32   `json:"temperature"`
-	Language       string    `json:"language"`
-	TranslateTo    string    `json:"translateTo"`
-	ReplaceOnExist bool      `json:"replaceOnExist"`
+	Provider       string    `yaml:"provider" json:"provider"`
+	InputPath      string    `yaml:"inputPath" json:"inputPath"`
+	SavePath       string    `yaml:"savePath" json:"savePath"`
+	Reader         io.Reader `yaml:"-" json:"-"`
+	Prompt         string    `yaml:"prompt" json:"prompt"`
+	Temperature    float32   `yaml:"temperature" json:"temperature"`
+	Language       string    `yaml:"language" json:"language"`
+	TranslateTo    string    `yaml:"translateTo" json:"translateTo"`
+	ReplaceOnExist bool      `yaml:"replaceOnExist" json:"replaceOnExist"`
 }
 
 type SubtitleOutput struct{}
