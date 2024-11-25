3f209f162850beed4e129b79002da694e7505998
Your Name <your_email@example.com>
2024-11-25 11:36:39 +0800

    增加stdio输出拦截

--- infra/logger.go
+++ infra/logger.go
@@ -1,24 +1,31 @@
 package infra
 
 import (
+	"io"
+	"os"
+
+	"m3u8dl_for_web/conf"
+
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
-	"io"
-	"m3u8dl_for_web/conf"
-	"os"
 )
 
 func InitLogger(conf *conf.Config) {
 	logFile := &lumberjack.Logger{
 		Filename: conf.Log.Path,
 		MaxSize:  conf.Log.MaxSize, // MB
-		//MaxBackups: conf.Log.LogNu,
+		// MaxBackups: conf.Log.LogNu,
 		MaxAge:    conf.Log.MaxAge, // days
 		Compress:  false,
 		LocalTime: true,
 	}
 
-	multiWriter := io.MultiWriter(logFile, os.Stdout)
+	
+	originalStdout, _, err := interceptStdio()
+	if err != nil {
+		panic(err)
+	}
+	multiWriter := io.MultiWriter(logFile, originalStdout)
 
 	logrus.SetFormatter(&logrus.TextFormatter{
 		DisableColors:   true,
@@ -28,6 +35,40 @@ func InitLogger(conf *conf.Config) {
 	logrus.SetLevel(logrus.DebugLevel)
 
 	logrus.SetReportCaller(true)
-	//Logger.SetOutput(os.Stdout)
+	// Logger.SetOutput(os.Stdout)
 	logrus.SetOutput(multiWriter)
+}
+
+// 能拦截golang的stdio输出,并不能拦截CGO调用动态链接库的输出
+func interceptStdio() (*os.File, *os.File, error) {
+	originalStdout := os.Stdout
+	originalStderr := os.Stderr
+	writer := logrus.StandardLogger().Writer()
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	os.Stdout = w
+
+	go func() {
+		for {
+			io.Copy(writer, r)
+		}
+	}()
+
+	stderrReader, stderrWriter, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	os.Stderr = stderrWriter
+
+	go func() {
+		for {
+			io.Copy(writer, stderrReader)
+		}
+	}()
+
+	return originalStdout, originalStderr, nil
 }
