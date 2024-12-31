1325e270a903b4baf49218da4d15c0e93258fe55
your name <example@example.com>
2024-12-31 17:11:13 +0800

    优化关键信息打印

--- service/subtitle/astisub_patch.go
+++ service/subtitle/astisub_patch.go
@@ -67,10 +67,11 @@ func (service *SubtitleService) ReGenerateBilingualSubtitleFromSegmentList(ctx c
 	if err != nil {
 		return err
 	}
+	logrus.Infof("subtitle file '%s' isBilingualSubtitle: %v ,segment len: %d", subtitlePath, isBilingualSubtitle, len(subtitleList))
+
 	if skipOnExists && isBilingualSubtitle {
 		return nil
 	}
-	logrus.Infof("start regenerate subtitle on %s", subtitlePath)
 
 	for _, segment := range subtitleList {
 		translatedText, err := service.translation.Translate(ctx, segment.Text, "", targetLang)
