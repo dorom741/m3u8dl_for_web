31dd2b33a5fd2686311f0070406cba44c97529ef
dorom <dorom@example.com>
2025-09-30 21:28:07 +0800

    优化翻译单元测试

--- service/translation/translate_test.go
+++ service/translation/translate_test.go
@@ -9,6 +9,14 @@ import (
 	"m3u8dl_for_web/infra"
 )
 
+var translateFragmentList = []string{
+	"He bore the cross and went to the bank",                           //他背着十字架走向河岸
+	"The pitcher will pitch on the pitch after the pitch.",             //投手将在场地准备工作后开始投球
+	"She left the rest on the left in the jar",                         //她把左边剩下的东西留在了罐子里
+	"The wind will wind around the windmill as we wind up the project", //当我们结束这个项目时，风会绕着风车吹
+	"goodbye", "happy", "love", "hate", "like", "dislike", "good", "bad", "beautiful",
+}
+
 func init() {
 	conf.InitConf("../../config.yaml")
 	infra.InitLogger(conf.ConfigInstance)
@@ -22,13 +30,7 @@ func TestDeepLXTranslate(t *testing.T) {
 
 	translation := NewDeepLXTranslation(conf.ConfigInstance.Translation.DeepLX, infra.DefaultHttpClient)
 
-	for i, word := range []string{
-		"He bore the cross and went to the bank",  //他背着十字架走向河岸
-		"The pitcher will pitch on the pitch after the pitch.", //投手将在场地准备工作后开始投球
-		"She left the left left in the can.", //她把左边剩下的东西留在了罐子里
-		"The wind will wind around the windmill as we wind up the project", //当我们结束这个项目时，风会绕着风车吹
-	    "goodbye", "happy", "love", "hate", "like", "dislike", "good", "bad", "beautiful",
-		} {
+	for i, word := range translateFragmentList {
 		result, err := translation.Translate(ctx, word, "en", "zh")
 		if err != nil {
 			t.Fatal(err)
@@ -43,7 +45,7 @@ func TestOpenaiTranslate(t *testing.T) {
 
 	translation := NewOpenAiCompatibleTranslation(conf.ConfigInstance.Translation.OpenAiCompatible)
 
-	for i, word := range []string{"computer", "car", "bye", "page", "big", "sad", "hello", "goodbye", "happy", "love", "hate", "like", "dislike", "good", "bad", "beautiful"} {
+	for i, word := range translateFragmentList {
 		result, err := translation.Translate(ctx, word, "en", "zh")
 		if err != nil {
 			t.Error(err)
